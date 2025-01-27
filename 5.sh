3e217b87c3617f960e74a57597404ca003469421
Mihailo Marcetic <mmarcetic@griddynamic.com>
2025-01-27 16:07:47 +0100

    Add checks for input file and option s

--- 5.sh
+++ 5.sh
@@ -9,6 +9,10 @@ while getopts "i:o:vs:rlu" opt; do
 	case $opt in
 		i)
 			INPUTFILE=$OPTARG
+			if [ ! -f $INPUTFILE ]; then
+					echo "Input file doesn't exist"
+					exit 1
+			fi
 			;;
 		o)
 			OUTPUTFILE=$OPTARG
@@ -21,16 +25,20 @@ while getopts "i:o:vs:rlu" opt; do
 			FIRST=1
 			;;
 		s)
-			if [ $FIRST -ne 0 ];then
-				INPUTFILE=$OUTPUTFILE
-			fi
-			echo "$FIRST"
-			WORDS=($OPTARG)
-			WORD_A=${WORDS[0]}
-			WORD_B=${WORDS[1]}
-			sed "s/$WORD_A/$WORD_B/g" "$INPUTFILE" > "$OUTPUTFILE"
-			FIRST=1
-			;;
+  		if [ $FIRST -ne 0 ]; then
+    		INPUTFILE=$OUTPUTFILE
+  		fi
+  		if [ -z "$OPTARG" ] || [ $(echo "$OPTARG" | awk '{print NF}') -ne 2 ]; then
+    		echo "Error: -s option requires two words to replace (e.g., -s 'word1 word2')"
+    		exit 1
+  		fi
+  		WORDS=($OPTARG)
+  		WORD_A=${WORDS[0]}
+  		WORD_B=${WORDS[1]}
+  		sed "s/$WORD_A/$WORD_B/g" "$INPUTFILE" > "$OUTPUTFILE"
+  		FIRST=1
+  		;;
+
 		l)
 			if [ $FIRST -ne 0 ];then
 				INPUTFILE=$OUTPUTFILE
